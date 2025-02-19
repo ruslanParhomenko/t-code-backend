e62867ee356c3e975c44cd24bdd0b2b7967bff00
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-02-19 15:28:29 +0200

    delete app\controller\service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ProductsModule } from './products/products.module';
 
 import { CorsMiddleware } from './cors.midlleware';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
